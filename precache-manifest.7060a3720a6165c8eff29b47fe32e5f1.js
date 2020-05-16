ce30a51828e5f1b31d6954bb06be4c86efda23e5
BarneyShi <barneyshek@gmail.com>
2020-05-16 04:19:32 -0700

    Updates

---
+++ precache-manifest.7060a3720a6165c8eff29b47fe32e5f1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "66d863e764318f24ca8a8c41ac63e2bc",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ff270b02f1a401d07dd4",
+    "url": "/static/css/main.77109a56.chunk.css"
+  },
+  {
+    "revision": "fe71087b63c956456fd8",
+    "url": "/static/js/2.d2d62fbd.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.d2d62fbd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ff270b02f1a401d07dd4",
+    "url": "/static/js/main.48ff83df.chunk.js"
+  },
+  {
+    "revision": "702554416a81a1e3a894",
+    "url": "/static/js/runtime-main.784c586e.js"
+  }
+]);
